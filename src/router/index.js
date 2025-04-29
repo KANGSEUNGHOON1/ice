b9f111fc0d5560c6d2fcac5fa8b6374d54c0ae36
DESKTOP-RLIRLHO\user <xgde0324@gmail.com>
2025-04-29 13:09:03 +0900

    제빙기 예제

---
+++ src/router/index.js
@@ -0,0 +1,94 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import Contact from "@/views/Contact.vue";
+import FAQ from "@/views/FAQ.vue";
+import Login from "@/views/Login.vue";
+import MyReservation from "@/views/MyReservation.vue";
+import ReservationForm from "@/views/ReservationForm.vue";
+import SeviceInfo from "@/views/SeviceInfo.vue";
+import ReservationStatus from "@/views/ReservationStatus.vue";
+import ModifyReservation from "@/views/ModifyReservation.vue";
+import Signup from "@/views/Signup.vue";
+import UserProfile from "@/views/UserProfile.vue";
+import FindPassword from "@/views/FindPassword.vue";
+// 기사페이지
+import DDashboard from "@/pages/worker/DDashboard.vue";
+import WorkerHome from "@/pages/worker/WorkerHome.vue";
+import AssingnedJobs from "@/pages/worker/AssingnedJobs.vue";
+import Assing from "@/pages/worker/Assing.vue";
+import Profile from "@/pages/worker/Profile.vue";
+import JobsHistory from "@/pages/worker/JobsHistory.vue";
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/contact", name: "Contact", component: Contact },
+  { path: "/seviceInfo", name: "SeviceInfo", component: SeviceInfo },
+  { path: "/faq", name: "FAQ", component: FAQ },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/signup", name: "Signup", component: Signup },
+  { path: "/my-reservations", name: "MyReservation", component: MyReservation },
+  {
+    path: "/reservation",
+    name: "ReservationForm",
+    component: ReservationForm,
+  },
+  {
+    path: "/find-password",
+    name: "FindPassword",
+    component: FindPassword,
+  },
+  {
+    path: "/profile",
+    name: "UserProfile",
+    component: UserProfile,
+  },
+  {
+    path: "/reservation-status/:id",
+    name: "ReservationStatus",
+    component: ReservationStatus,
+  },
+  {
+    path: "/modify-reservation/:id",
+    name: "ModifyReservation",
+    component: ModifyReservation,
+  },
+  // 기사페이지
+  {
+    path: "/worker",
+    component: WorkerHome,
+    redirect: "/worker/ddashboard", //경로 접근시 자동 리다이렉트
+    children: [
+      {
+        path: "ddashboard",
+        component: DDashboard,
+      },
+      {
+        path: "workerHome",
+        component: WorkerHome,
+      },
+      {
+        path: "AssingnedJobs",
+        component: AssingnedJobs,
+      },
+      {
+        path: "Assing",
+        component: Assing,
+      },
+      {
+        path: "Profile",
+        component: Profile,
+      },
+      {
+        path: "JobsHistory",
+        component: JobsHistory,
+      },
+     
+
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router;
